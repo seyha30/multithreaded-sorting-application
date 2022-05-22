b680a9231e73880968bb246078806fea8f3a8d90
seyha30 <57175099+seyha30@users.noreply.github.com>
2022-05-22 14:48:50 +0700

    1

--- src/com/MainProgram.java
+++ src/com/MainProgram.java
@@ -7,9 +7,10 @@ public class MainProgram {
 	public static void main(String[] args) {
 		int lst[] = { 7, 12, 19, 3, 18, 4, 2, 6, 15, 8 };
 		int length = lst.length - 1;
+		long startTime = System.nanoTime();
 		for (int i = 0; i <= length; i++) {
 			for (int j = 0; j <= length - 1; j++) {
-                int temp = lst[j];
+				int temp = lst[j];
 				if (lst[j] > lst[j + 1]) {
 					lst[j] = lst[j + 1];
 					lst[j + 1] = temp;
@@ -18,7 +19,9 @@ public class MainProgram {
 			}
 
 		}
-		System.out.println(" Array after sort " + Arrays.toString(lst));
+		long endTime = System.nanoTime();
+		System.out.println("Exceution time " + (endTime - startTime) + " in milliseconds");
+		System.out.println("Array after sort " + Arrays.toString(lst));
 
 	}
 
