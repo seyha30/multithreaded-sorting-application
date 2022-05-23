8f84771c115a733db781abe761e321ecb0dd26e9
seyha <sinseyha1x@gmail.com>
2022-05-23 09:37:36 +0700

    add

--- src/com/MainProgram.java
+++ src/com/MainProgram.java
@@ -8,27 +8,37 @@ public class MainProgram {
 		int list[] = { 7, 12, 19, 3, 18, 4, 2, 6, 15, 8 };
 		int length = list.length - 1;
 		long startTime = System.nanoTime();
-		for (int i = 0; i <= length; i++) {
-			for (int j = 0; j <= length - 1; j++) {
-				if (list[j] >= list[j + 1]) {
-					int temp = list[j];
-					list[j] = list[j + 1];
-					list[j + 1] = temp;
-
-				}
+		/*
+		 * for (int i = 0; i <= length; i++) { for (int j = 0; j <= length - 1; j++) {
+		 * if (list[j] >= list[j + 1]) { int temp = list[j]; list[j] = list[j + 1];
+		 * list[j + 1] = temp;
+		 * 
+		 * } }
+		 * 
+		 * }
+		 */
+//		long endTime = System.nanoTime();
+//		System.out.println("Execution time " + (endTime - startTime) + " in milliseconds");
+//		System.out.println("Array after sort " + Arrays.toString(list));
+		int[] firstArray = new int[(list.length + 1) / 2];
+		int[] secondArray = new int[list.length - firstArray.length];
+		for (int i = 0; i < list.length; i++) {
+			if (i < firstArray.length) {
+				firstArray[i] = list[i];
+			} else {
+				secondArray[i - firstArray.length] = list[i];
 			}
-
 		}
-		long endTime = System.nanoTime();
-		System.out.println("Exceution time " + (endTime - startTime) + " in milliseconds");
-		System.out.println("Array after sort " + Arrays.toString(list));
-		
-		
-//		start thread
-		FirstThread firstThread = new FirstThread(list);
-		SecondThread secondThread = new SecondThread(list);
-		
+		System.out.println("Array 1" + Arrays.toString(firstArray));
+		System.out.println("Array 2" + Arrays.toString(secondArray));
 
+//		start thread
+		FirstThread firstThread = new FirstThread(firstArray);
+		firstThread.start();
+		SecondThread secondThread = new SecondThread(secondArray);
+		secondThread.start();
+		ThirdThread thirdThread = new ThirdThread(firstThread, secondThread);
+		thirdThread.start();
 	}
 
 }
